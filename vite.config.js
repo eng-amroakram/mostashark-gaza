2ab3cdf9e5b86a15d894e93d2cdd3deccdd2554a
eng-amroakram <eng.amroakram@gmail.com>
2025-11-20 12:48:58 +0200

    new update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,              // يسمح بالوصول من الخارج
+    port: 5173,              // نفس المنفذ الافتراضي
+    strictPort: true,        // استخدم نفس المنفذ فقط
+    hmr: {
+      host: "45.130.104.108", // ضع هنا IP السيرفر
+      port: 5173
+    }
+  }
 })
