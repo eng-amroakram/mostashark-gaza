afbb00ec746804921635d12120151e724b0316b4
eng-amroakram <eng.amroakram@gmail.com>
2025-11-20 13:23:35 +0200

    new update

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,              // يسمح بالوصول من الخارج
-    port: 5173,              // نفس المنفذ الافتراضي
-    strictPort: true,        // استخدم نفس المنفذ فقط
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['mostashark-gaza.com', 'www.mostashark-gaza.com'],
     hmr: {
-      host: "45.130.104.108", // ضع هنا IP السيرفر
+      host: "mostashark-gaza.com",  // الافضل استخدام الدومين بدل الـ IP
       port: 5173
     }
   }
